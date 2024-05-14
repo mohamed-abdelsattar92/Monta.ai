20cc84221db3503edca32c37c06bb34baf6ebd63
Mohamed Abdel Sattar <mohamed.abdelsattar@tempo.fit>
2024-05-15 01:03:40 +0300

    Remove Enums for simplicity

--- montaai/montaai/models/messages.py
+++ montaai/montaai/models/messages.py
@@ -1,18 +1,13 @@
-from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey, Enum as EnumField
+from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey, String
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from montaai.helpers.database import db
-import enum
-
-
-class MessageRole(enum.Enum):
-    USER = "user"
 
 
 class Message(db.Model):
     id = Column(Integer, primary_key=True)
     conversation_id = Column(Integer, ForeignKey("conversation.id"))
     content = Column(Text)
-    role = db.Column(EnumField(MessageRole), default=MessageRole.USER, nullable=False)
+    role = Column(String(80), nullable=False)
     timestamp = Column(DateTime, default=datetime.utcnow)
     conversation = relationship("Conversation", back_populates="messages")
